21f0eca22b4e806ac835b61a61505eb8bbec5c00
Sourav Samaddar <samads2@medtronic.com>
2026-02-19 12:37:11 +0530

    deleted parser.cs code

--- XamlToHtmlConverter/Program.cs
+++ XamlToHtmlConverter/Program.cs
@@ -4,27 +4,6 @@ using System.IO;
 using XamlToHtmlConverter.Parsing;
 
 
-//class program
-//{
-//    static void Main(string[] args)
-//    {
-//        var parser = new XamlParser();
-//        var ir = parser.Parse("sample.xaml");
-//        printIr(ir, 0);
-//    }
-
-//    static void printIr(IrElement element, int indent)
-//    {
-//        Console.WriteLine($"{new string(' ', indent)}{element.Type}");
-
-//        foreach (var prop in element.Properties)
-//            Console.WriteLine($"{new string(' ', indent + 2)}Prop: {prop.Key}={prop.Value}");
-//        foreach (var attached in element.AttachedProperties)
-//            Console.WriteLine($"{new string(' ', indent + 2)}Attached: {attached.Key}={attached.Value}");
-//        foreach (var child in element.Children)
-//            printIr(child, indent + 2);
-//    }
-//}
 
 class Program
 {
